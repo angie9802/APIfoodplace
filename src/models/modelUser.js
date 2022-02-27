b90f126d04cba1520ba3e1afe5246d78a35858ea
Angie Cantor <avbarajasc@unal.edu.co>
2022-02-27 17:18:31 -0500

    group by category missing

--- src/models/modelUser.js
+++ src/models/modelUser.js
@@ -21,53 +21,7 @@ const User = {
         }
         
     },
-    findByField : async(field, text)=>{
-      try {
-        const allUsers = await db.Users.findAll()
-        const userFound = await allUsers.find(user => user[field] === text)
-        return userFound
-      } catch (err) {
-        console.log(err)
-        return err
-      }
-    },
-    create : async (userData) =>{
-      try {
-        let newUser = await db.Users.create({
-          userimage: userData.userImage,
-          Roles_id: userData.role,
-          ...userData
-        })
-      } catch(err) {
-        console.log(err)
-      }
-    }, 
-    update: async (id, user) => {
-      try {
-        let userUpdate = {
-          ...user
-        }
-        await db.Users.update(userUpdate, {
-          where: {
-            id: id
-          }
-        })
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    destroy: async (id) => {
-        try{
-             await db.Users.destroy({
-                where : {
-                    id :id
-                }
-            })
-        }catch(err){
-            console.log(err)
-        }
-       
-    }
+   
 }
 module.exports = User
 
